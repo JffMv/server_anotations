98f8d2bf4fbf20fdd3d368720e6b981317ba60d5
Jefferson Mesa <mesjefer2@gmail.com>
2025-02-08 16:12:35 -0500

    solve some troble

--- src/main/java/co/edu/eci/GetMapping.java
+++ src/main/java/co/edu/eci/GetMapping.java
@@ -1,5 +1,13 @@
 package co.edu.eci;
 
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.METHOD)
 public @interface GetMapping {
     public String value(); // value para que cuando use GetMapping("") pueda pasarle el paramero sin tener que decir que es ese paramentor
 
